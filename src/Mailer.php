4db70dbd7e5c618adb88e48ca241aa43389643c0
Sebastian Bergmann <sb@sebastian-bergmann.de>
2010-11-01 10:09:14 -0700

    Add disclaimer.

--- src/Mailer.php
+++ src/Mailer.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * This example code is no production code, it is used for training purposes.
+ * The code deliberately has architectural problems and potential security problems.
+ *
+ * Copyright (c) 2010 Sebastian Bergmann, Stefan Priebsch
+ * http://thePHP.cc
+ */
+
 class Mailer
 {
     public function sendMail($email, $subject, $body)
